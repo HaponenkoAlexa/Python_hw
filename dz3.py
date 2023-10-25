13098059ba576f340c669f0c52f8ac7ec3f8f827
Alexa <gaponenko.sasha8@gmail.com>
2023-10-25 19:21:00 +0300

    dz2-3

--- dz3.py
+++ dz3.py
@@ -5,6 +5,5 @@ digit_2 = (number % 10000) // 1000
 digit_3 = (number % 1000) // 100
 digit_4 = (number % 100) // 10
 digit_5 = number % 10
+(print(digit_5 * 10000 + digit_4  * 1000 + digit_3 * 100 + digit_2 *10 + digit_1, ))
 
-
-print(digit_5, digit_4, digit_3, digit_2, digit_1, )
