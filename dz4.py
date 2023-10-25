200c8c5be47b4d2f75d8a04815fa7a0c8bdb6c6e
Alexa <gaponenko.sasha8@gmail.com>
2023-10-25 20:25:23 +0300

    dz4

---
+++ dz4.py
@@ -0,0 +1,19 @@
+x = int(input("Введіть число: "))
+action = input('Введіть дію (+, -, *, /):')
+z = int(input("Введіть число: "))
+
+if action == "+":
+    result = x + z
+elif action == "-":
+    result = x - z
+elif action == "*":
+    result = x * z
+elif action == "/":
+    if z != 0:
+        result = x / z
+    else:
+        print("Ділення на нуль неможливе.")
+        result = None
+
+if result is not None:
+    print("Результат:", result)
