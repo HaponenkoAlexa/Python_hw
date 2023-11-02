22ade6def9230e7cf46ae232909631b8b24f842c
Alexa <gaponenko.sasha8@gmail.com>
2023-11-02 19:49:41 +0200

    dz5

--- dz6.py
+++ dz6.py
@@ -1,5 +1,11 @@
-my_list = [9, 8, 7, 55]
+my_list = []
 
-last_element = my_list.pop()
-my_list.insert(0, last_element)
-print(my_list)
+if my_list:
+    last_element = my_list.pop()
+    my_list.insert(0, last_element)
+    print(my_list)
+else:
+    my_list = []
+    print(my_list)
+
+
