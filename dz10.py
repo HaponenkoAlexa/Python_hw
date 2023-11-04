6a3177b166f19362bf31a8dba80d497b183b7537
Alexa <gaponenko.sasha8@gmail.com>
2023-11-04 19:56:01 +0200

    dz5

---
+++ dz10.py
@@ -0,0 +1,15 @@
+import string
+import keyword
+import string
+
+my_variable = input("Введіть змінну: ")
+
+if len(my_variable) == 0 or my_variable[0].isdigit():
+    print(False)
+else:
+    if not all(char in string.ascii_letters + string.digits + "_" for char in my_variable):
+        print(False)
+    elif my_variable in keyword.kwlist:
+        print(False)
+    else:
+        print(True)
