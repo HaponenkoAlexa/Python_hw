3d4ee37a410988e22a282abc05651063d0239cfe
Alexa <gaponenko.sasha8@gmail.com>
2023-11-06 18:09:23 +0200

    dz5

--- dz10.py
+++ dz10.py
@@ -1,8 +1,13 @@
+import string
 import keyword
 
 my_variable = input("Введіть змінну: ")
 
-if len(my_variable) == 0 or my_variable[0].isdigit() or not my_variable.islower():
+if len(my_variable) == 0:
+    print(False)
+elif my_variable == "_":
+    print(True)
+elif my_variable[0].isdigit() or not my_variable.islower():
     print(False)
 elif not all(char in string.ascii_letters + string.digits + "_" for char in my_variable):
     print(False)
