c3d946eeccdc0287fffcb490dcafe4dca0f4ab14
Alexa <gaponenko.sasha8@gmail.com>
2023-11-06 17:56:45 +0200

    dz5

--- dz12.py
+++ dz12.py
@@ -1,11 +1,10 @@
 import string
 
 my_hus = input('Введіть назву для хештега: ')
-
+my_hus = my_hus[:140]
 if my_hus:
     my_hus = ''.join(word.title() for word in my_hus.split() if word.isalpha())
-
-    if all(char not in string.punctuation for char in my_hus) and len(my_hus) <= 140:
+    if all(char not in string.punctuation for char in my_hus):
         tmp = f"#{my_hus}"
         my_hus = tmp
         print(my_hus)
