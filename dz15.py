0ea1054c21630279b06e680f642532a2b538a152
Alexa <gaponenko.sasha8@gmail.com>
2023-11-09 20:44:06 +0200

    dz5

--- dz15.py
+++ dz15.py
@@ -5,7 +5,17 @@ if 0 <= number <= 8640000:
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
 
-    formatted_time = f"{days} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+    if days == 0:
+        day_word = "днів"
+    elif days == 1:
+        day_word = "день"
+    elif days // 10 == 1 or days % 10 in (2, 3, 4):
+        day_word = "дні"
+    else:
+        day_word = "днів"
+
+    formatted_time = f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
     print(formatted_time)
 else:
     print(False)
+
