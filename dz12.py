c2446ca74310c2c42d243593998248dbf61f7f9d
Alexa <gaponenko.sasha8@gmail.com>
2023-11-09 11:14:43 +0200

    dz5

--- dz12.py
+++ dz12.py
@@ -4,7 +4,7 @@ my_hus = input('Введіть назву для хештега: ')
 if my_hus:
     my_hus = ''.join(word.title() for word in my_hus.split() if word.isalpha())
     my_hus = f"#{my_hus}"
-    my_hus = my_hus[:10]  # обмежуємо довжину хештега до 140 символів
+    my_hus = my_hus[:140]  # обмежуємо довжину хештега до 140 символів
     print(f"Ваш хештег: {my_hus}")
 else:
     print("Ви ввели порожню строку.")
