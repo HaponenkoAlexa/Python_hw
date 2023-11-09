22c86d52609cb1d20845a81de2f603c19daabb55
Alexa <gaponenko.sasha8@gmail.com>
2023-11-09 11:10:58 +0200

    dz5

--- dz12.py
+++ dz12.py
@@ -1,15 +1,15 @@
 import string
 
 my_hus = input('Введіть назву для хештега: ')
-my_hus = my_hus.strip()[:140]
 if my_hus:
     my_hus = ''.join(word.title() for word in my_hus.split() if word.isalpha())
     if all(char not in string.punctuation for char in my_hus):
-        tmp = f"#{my_hus}"
+        tmp = f"#{my_hus.strip()[:140]}"
         my_hus = tmp
-        print(my_hus)
+        print(f"Ваш хештег: {my_hus}")
     else:
-        print(False)
+        print("Хештег не може містити символи пунктуації, будь ласка, введіть іншу назву.")
 else:
     print("Ви ввели порожню строку.")
+
 
