0a02d5c429608ab44278b916214c5bc6e0ad910d
Alexa <gaponenko.sasha8@gmail.com>
2023-11-13 20:45:17 +0200

    dz5

--- dz15.py
+++ dz15.py
@@ -7,9 +7,9 @@ if 0 <= number <= 8640000:
 
     if days == 0:
         day_word = "днів"
-    elif days == 1:
+    elif days == 1 or (days % 10 == 1 and days % 100 != 11):
         day_word = "день"
-    elif days // 10 == 1 or days % 10 in (2, 3, 4):
+    elif 2 <= days % 10 <= 4 and (days % 100 < 10 or days % 100 >= 20):
         day_word = "дні"
     else:
         day_word = "днів"
