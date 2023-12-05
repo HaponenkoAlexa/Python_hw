26cd4196931706abfc8d5a3a1874dbfa6048d3fc
Alexa <gaponenko.sasha8@gmail.com>
2023-12-05 15:57:11 +0200

    dz

--- dz37.py
+++ dz37.py
@@ -7,24 +7,34 @@ class Rectangle:
         return self.width * self.height
 
     def __eq__(self, other):
-        return self.get_square() == other.get_square()
+        return (self.width, self.height) == (other.width, other.height)
 
     def __add__(self, other):
-        new_width = max(self.width, other.width)
-        new_height = max(self.height, other.height)
-        return Rectangle(new_width, new_height)
+        if isinstance(other, Rectangle):
+            width_sum = self.width + other.width
+            height_sum = self.height + other.height
+            return Rectangle(width_sum, height_sum)
+        else:
+            raise ValueError("Unsupported operand type for +: {}".format(type(other)))
 
     def __mul__(self, n):
-        return Rectangle(self.width * n, self.height * n)
+        if isinstance(n, (int, float)):
+            return Rectangle(self.width * n, self.height * n)
+        else:
+            raise ValueError("Unsupported operand type for *: {}".format(type(n)))
 
     def __str__(self):
-        return f'Rectangle: width={self.width}, height={self.height}, square={self.get_square()}'
+        return f"Rectangle: width={self.width}, height={self.height}"
 
 
-# Тестування
+# Приклад використання:
 r1 = Rectangle(2, 4)
-r4 = r1 * 2  # Adjust the multiplier to 2
-assert r4.get_square() == 16, 'Test4'  # Corrected value
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
 
-print(r1)
-print(r4)
+r3 = r1 + r2
+assert r3.get_square() == 26, 'Test3'
+
+r4 = r1 * 4
+assert r4.get_square() == 32, 'Test4'
