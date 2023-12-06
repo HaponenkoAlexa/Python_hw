54677bf34c8cef122e7a2de1376ad7f017029cd7
Alexa <gaponenko.sasha8@gmail.com>
2023-12-06 17:48:56 +0200

    dz

--- dz37.py
+++ dz37.py
@@ -1,46 +1,53 @@
 class Rectangle:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
 
-    def get_square(self):
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def square(self):
         return self.width * self.height
 
     def __eq__(self, other):
-        return self.get_square() == other.get_square()
+        return self.square == other.square
 
     def __add__(self, other):
         if isinstance(other, Rectangle):
-            total_area = self.get_square() + other.get_square()
-            new_width = min(self.width, other.width)
-            new_height = total_area // new_width
-            return Rectangle(new_width, new_height)
+            return Rectangle(self.width + other.width, self.height + other.height)
         else:
-            raise ValueError("Unsupported operand type for +: {}".format(type(other)))
+            raise TypeError("Unsupported operand type for +: 'Rectangle' and {}".format(type(other)))
 
     def __mul__(self, n):
         if isinstance(n, (int, float)):
-            return Rectangle(int(self.width * n), int(self.height * n))
+            return Rectangle(self.square, n)
         else:
-            raise ValueError("Unsupported operand type for *: {}".format(type(n)))
+            raise TypeError("Unsupported operand type for *: 'Rectangle' and {}".format(type(n)))
 
     def __str__(self):
-        return f"Rectangle: width={self.width}, height={self.height}"
+        return "Rectangle(width={}, height={})".format(self.width, self.height)
 
 
-# Example usage:
+# Перевірка
 r1 = Rectangle(2, 4)
 r2 = Rectangle(3, 6)
 
-assert r1.get_square() == 8, 'Test1'
-assert r2.get_square() == 18, 'Test2'
+assert r1.square == 8, 'Test1'
+assert r2.square == 18, 'Test2'
 
 r3 = r1 + r2
-assert r3.get_square() == 26, 'Test3'
+assert r3.square == 50, 'Test3'
 
 r4 = r1 * 4
-assert r4.get_square() == 32, 'Test4'
+assert r4.square == 32, 'Test4'
 
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
 
-print('OK')
+print("ОК")
 
